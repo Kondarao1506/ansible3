623d8a68ef0435e079f0d27c0de300ebf4f94a4f
Kondarao1506 <Kondarao1506@gmail.com>
2025-04-20 17:31:28 +0530

    shell script for ansible install

---
+++ 17_shell_ansible.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 FAILED........"
+    else
+        echo "$2 SUCCESS........"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo: "Please run with root priviliges like....sh $0"
+    exit 1
+fi
+
+echo "Please enter the package to install"
+read PACKAGE
+
+dnf installed $PACKAGE 
+if [ $? -ne 0 ]
+then    
+    echo "Installing $PACKAGE .........."
+    dnf install $PACKAGE -y
+    VALIDATE $? "installing $PACKAGE...."
+else
+    else: "$PACKAGE is already installed"
+fi
+
